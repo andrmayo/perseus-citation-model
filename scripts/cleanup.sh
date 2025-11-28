6252a2f5390ca281190a512fa3f257844a75d0e6
Andrew Mayo <acmayo399@gmail.com>
2025-11-28 04:51:11 -0500

    edits to scripts

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
-echo "cleaning up files in rm $ROOT_DIR/model_data/"
-rm "$ROOT_DIR/model_data/"*
+echo "cleaning up files in rm $ROOT_DIR/model_data/extraction/"
+rm "$ROOT_DIR/model_data/extraction/"*
 echo "cleaning up files and directories in $ROOT_DIR/outputs/extraction/"
 rm -rf "$ROOT_DIR/outputs/extraction/"*
 echo "cleaning up files in $ROOT_DIR/outouts/models/extraction/"
