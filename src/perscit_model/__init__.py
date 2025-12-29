61011fe127a430ce20cf12ac7c6f048aae2e904e
andrmayo <acmayo399@gmail.com>
2025-12-29 16:08:12 -0500

    fixed reentrant issue with multiprocessing

--- src/perscit_model/__init__.py
+++ src/perscit_model/__init__.py
@@ -5,8 +5,9 @@ import multiprocessing
 # Set multiprocessing start method to 'spawn' to avoid fork() issues with CUDA
 # and multi-threaded libraries like PyTorch. This prevents deadlocks and
 # "Cannot re-initialize CUDA in forked subprocess" errors.
+# NOTE: Don't use force=True - it causes reentrant ResourceTracker cleanup issues
 try:
-    multiprocessing.set_start_method('spawn', force=True)
+    multiprocessing.set_start_method('spawn')
 except RuntimeError:
     # Start method already set (e.g., by another import), ignore
     pass
@@ -14,7 +15,7 @@ except RuntimeError:
 # Also set for multiprocess library (fork of multiprocessing used by some libraries)
 try:
     import multiprocess
-    multiprocess.set_start_method('spawn', force=True)
+    multiprocess.set_start_method('spawn')
 except (ImportError, RuntimeError):
     # multiprocess not installed or start method already set
     pass
